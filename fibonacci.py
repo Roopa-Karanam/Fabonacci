0c1452b4814a293794422d48baf446d595800e99
Roopa-Karanam <roopa.karanam@mohawkcollege.ca>
2022-07-28 22:40:25 -0400

    fixedtth epylint errors

--- fibonacci.py
+++ fibonacci.py
@@ -13,17 +13,15 @@ class fibonacci:
         self.number=number
         self.a=0
         self.b=1
-        self.count=-1;
+        self.count=-1
     def __iter__(self):
         '''returns the instane object whch is an iterator'''
         return self
     def __next__(self):
-        '''defines the instance object as an itereator'''
-        
+        '''defines the instance object as an itereator''' 
         self.count+=1
         if self.count>self.number:
             raise StopIteration
-
         if self.count==1:
             return 1
         elif self.count==0:
@@ -34,5 +32,4 @@ class fibonacci:
             self.c=self.a+self.b
             self.a=self.b
             self.b=self.c
-            return (self.c)
-           
+            return self.c
--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -1,27 +1,27 @@
-from fibonacci import fibonacci
+"""testing the fibonacci series"""
 import pytest
+from fibonacci import fibonacci
 
 def test_fibonacci_init_():
+    '''testing init'''
     assert list(fibonacci(0))==[0]
 def test_with_string():
+    '''testing string'''
     with pytest.raises(ValueError):
         fibonacci("test")
 def test_one():
+    '''testing with one'''
     assert list(fibonacci(1))==[0,1]
 def test_two():
+    '''testing with 2'''
     assert list(fibonacci(2))==[0,1,1]
 def test_four():
+    '''testing with 4'''
     assert list(fibonacci(4))==[0,1,1,2,3]
 def test_ten():
+    '''testing with 10'''
     assert list(fibonacci(10))==[0,1,1,2,3,5,8,13,21,34,55]
-def test_one():
-    assert list(fibonacci(1))==[0,1]
 def test_negative():
+    '''testing negative'''
     assert list(fibonacci(-1))==[]
-
-
-
-
-
     
-  

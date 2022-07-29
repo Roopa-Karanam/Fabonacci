5600d7e1ba0bc3aeadc6c49a0f499ef9edbe9a41
Roopa-Karanam <roopa.karanam@mohawkcollege.ca>
2022-07-28 21:58:02 -0400

    THe stest for one , two , and four passed

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -6,6 +6,14 @@ def test_fibonacci_init_():
 def test_with_string():
     with pytest.raises(ValueError):
         fibonacci("test")
+def test_one():
+    assert list(fibonacci(1))==[0,1]
+def test_two():
+    assert list(fibonacci(2))==[0,1,1]
+def test_four():
+    assert list(fibonacci(4))==[0,1,1,2,3]
+
+
 
     
   
